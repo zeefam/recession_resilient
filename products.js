6c606f79df60df8d83111935dd8d5194c3e024a5
zeefam <raymondbzialcita@gmail.com>
2025-05-02 12:36:44 -0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,22 @@
+// Array of product objects
+const products = [
+    {
+        id: 1,
+        name: "Product One",
+        description: "This is a fantastic product that solves many problems.",
+        image: "images/product1.jpg",
+        url: "https://affiliate-link.com/product1",
+        rating: 4.5,
+        price: "$49.99"
+    },
+    {
+        id: 2,
+        name: "Product Two",
+        description: "The second product is even better than the first!",
+        image: "images/product2.jpg",
+        url: "https://affiliate-link.com/product2",
+        rating: 5,
+        price: "$29.99"
+    },
+    // Add more products as needed
+];
