3e147538a869d75c7d4ec282b8e0e84bcb0925c4
zeefam <raymondbzialcita@gmail.com>
2025-05-02 13:03:44 -0700

    Update products.js

--- products.js
+++ products.js
@@ -2,21 +2,42 @@
 const products = [
     {
         id: 1,
-        name: "Product One",
-        description: "This is a fantastic product that solves many problems.",
-        image: "images/product1.jpg",
-        url: "https://affiliate-link.com/product1",
-        rating: 4.5,
-        price: "$49.99"
+        name: "Restored Apple MacBook Pro Core i5",
+        image: "https://i5.walmartimages.com/seo/Restored-Apple-MacBook-Pro-Core-i5-13-3-inch-512GB-SSD-8GB-RAM-Intel-HD-Graphics-Bundle-USA-s-Bluetooth-Wireless-Airbuds-Wireless-Mouse-Blac_c51d9ebf-2ff5-442a-84c4-af845a83c7d5.0c60c44becc8300fa245afdebde3ef5e.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF",
+        originalPrice: "$409",
+        currentPrice: "$269",
+        saveAmount: "-34%",
+        description: "Factory refurbished with 1.10GHz/8GB/512GB. Perfect for your online business, or education.",
+        url: "https://www.walmart.com/ip/Restored-Apple-MacBook-Pro-Core-i5-13-3-inch-512GB-SSD-8GB-RAM-Intel-HD-Graphics-Bundle-USA-s-Bluetooth-Wireless-Airbuds-Wireless-Mouse-Blac/789736467?conditionGroupCode=2&classType=REGULAR&adsRedirect=true"
     },
     {
         id: 2,
-        name: "Product Two",
-        description: "The second product is even better than the first!",
-        image: "images/product2.jpg",
-        url: "https://affiliate-link.com/product2",
-        rating: 5,
-        price: "$29.99"
+        name: "Raised Garden Bed Wooden Planter Box",
+        image: "https://i5.walmartimages.com/seo/PETSCOSSET-8x2Ft-Raised-Garden-Bed-Wooden-Planter-Box-Vegetable-Garden-Box-for-Outdoor-Gardening_364b3470-69f6-4b5f-a284-03ea8b0c6d03.e5cefbb4bbd596d9069db371cef3b417.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF",
+        originalPrice: "$95",
+        currentPrice: "$59",
+        saveAmount: "-38%",
+        description: "Durable 8x2ft raised garden bed with easy assembly and versatile design!",
+        url: "https://www.walmart.com/ip/PETSCOSSET-8x2Ft-Raised-Garden-Bed-Wooden-Planter-Box-Vegetable-Garden-Box-for-Outdoor-Gardening/5262026054?adsRedirect=true"
     },
-    // Add more products as needed
+    {
+        id: 3,
+        name: "50pc Airtight Food Storage Set",
+        image: "https://i5.walmartimages.com/seo/50-Pcs-Food-Storage-Containers-Lids-Airtight-Plastic-Meal-Prep-Container-Pantry-Kitchen-Organization-BPA-Free-Leak-Proof-Labels-Marker-Pen_cecaef1d-c00d-4105-a857-64cf43b9806a.68982087cdea901720e4059e3ddd17df.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF",
+        originalPrice: "$65",
+        currentPrice: "$30",
+        saveAmount: "-54%",
+        description: "Save on food costs with this set of 50 food storage containers for stockpiling & meal prep! Durable, BPA-free, & airtight!",
+        url: "https://www.walmart.com/ip/50-Pcs-Food-Storage-Containers-Lids-Airtight-Plastic-Meal-Prep-Container-Pantry-Kitchen-Organization-BPA-Free-Leak-Proof-Labels-Marker-Pen/9338261680?classType=REGULAR&athbdg=L1900&adsRedirect=true"
+    },
+    {
+        id: 4,
+        name: "Versace Perfume 3oz.",
+        image: "https://i5.walmartimages.com/seo/Versace-Bright-Crystal-Absolu-Eau-De-Parfum-for-Women-3-oz_09522e35-24ed-4732-85a1-c97d56a589e0.2e07b2d2d32680c7211feec7de6ed230.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF",
+        originalPrice: "$130",
+        currentPrice: "$65",
+        saveAmount: "-50%",
+        description: "Versace Bright Crystal Absolu Eau De Parfum Spray for Women 3 oz",
+        url: "https://www.walmart.com/ip/Versace-Bright-Crystal-Absolu-Eau-De-Parfum-for-Women-3-oz/47002984"
+    }
 ];
